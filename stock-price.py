36e4a89630b92f3d215005e39494092c1ba5ba31
JuneChen1 <june850713@gmail.com>
2026-02-21 18:01:22 +0800

    add function to get stock info

--- stock-price.py
+++ stock-price.py
@@ -1,15 +1,26 @@
 import requests
 import datetime
 import math
-# 上櫃 https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=otc_5508.tw&json=1&delay=0&_=1771661295326&lang=zh_tw
-# 上市 https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_0050.tw&json=1&delay=0&_=1771658259143&lang=zh_tw
+
+time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+
+def getStockInfo(symbol):
+  url = 'https://mis.twse.com.tw/stock/api/getStockNames.jsp?'
+  payload = {
+    'n': str(symbol),
+    '_': time
+  }
+  response = requests.get(url, params=payload)
+  if response.status_code == 200:
+    data = response.json()
+    result = data['datas'][0]['key'][:4]
+  return result
 
 def getStockPrice(symbol):
-  time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-  
+  temp = getStockInfo(symbol)
   url = 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp'
   payload = {
-    'ex_ch': 'tse_' + str(symbol) + '.tw',
+    'ex_ch': temp + str(symbol) + '.tw',
     'json': '1',
     'delay': '0',
     '_': time
@@ -17,7 +28,8 @@ def getStockPrice(symbol):
   response = requests.get(url, params=payload)
   if response.status_code == 200:
     data = response.json()
-    return data['msgArray'][0]['pz']
+    price = data['msgArray'][0]['pz']
+  return price 
 
 price = getStockPrice('0050')
 print(price)
