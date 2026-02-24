6acb6abe160373e05d8c98c78ee68210ade1d927
JuneChen1 <june850713@gmail.com>
2026-02-24 13:12:15 +0800

    Update getStockPrice function when price return '-'

--- stock-price.py
+++ stock-price.py
@@ -22,9 +22,9 @@ def getStockInfo(symbol):
 def getStockPrice(symbol):
   temp = getStockInfo(symbol)
   if temp == 'Not Found':
-      price = 'Not Found'
-      name = 'Not Found'
-      return price, name
+    price = 'Not Found'
+    name = 'Not Found'
+    return price, name
   
   url = 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp'
   payload = {
@@ -38,17 +38,24 @@ def getStockPrice(symbol):
   if response.status_code == 200:
     data = response.json()
     try:
-      price = data['msgArray'][0]['pz']
-      name = data['msgArray'][0]['n']
+      price = data['msgArray'][0]['z']
+      if price == '-':
+        price = data['msgArray'][0]['a'].split('_')[0]
     except:
       price = 'Not Found'
+
+    try:
+      name = data['msgArray'][0]['n']
+    except:
       name = 'Not Found'
+
   return price, name
 
 symbol = input('股票代號：')
 price, name = getStockPrice(symbol)
-if price == 'Not Found' and name == 'Not Found':
-  print('查無此股票代號')
+
+print('股票名稱：', name, sep='')
+if price == 'Not Found':
+  print('股價：', price, sep='')
 else:
-  print('股票名稱：', name, sep='')
   print('股價：', f"{float(price):.2f}", sep='')
